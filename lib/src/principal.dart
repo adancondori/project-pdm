626e62a4b3a5ebdc82af4e0a25b01b8e244c47a1
adancondori <adancondoric@gmail.com>
2022-10-26 17:25:01 -0400

    add drawer in principal screen

--- lib/src/principal.dart
+++ lib/src/principal.dart
@@ -29,6 +29,37 @@ class _ListExampleState extends State<ListExample> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Actividades')),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
+      ),
       body: Container(
         // verde
         color: Colors.white,
